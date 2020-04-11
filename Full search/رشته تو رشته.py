0689ec0591c00305189d8351269c43ce5b0d45ae
Mohammad reza golsorkhi <mgol2077@outlook.com>
2020-04-12 00:34:31 +0430

    all

---
+++ Full search/رشته تو رشته.py
@@ -0,0 +1,10 @@
+i=input()
+i2=input()
+t=0
+for x in range(len(i)):
+    t=i.find(i2,x)
+    if not t==-1 and x==t:
+       print(t+1)
+      
+
+       
---
+++ sort/sort.py
@@ -0,0 +1,5 @@
+i1=int(input())
+is1=list(map(int, input().split()))
+#is1 = [1,4,100,10]
+is1.sort(reverse=True)
+print(*is1)
